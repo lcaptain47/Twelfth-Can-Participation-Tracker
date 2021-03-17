1f2eb0a8587558a4f1973b851cbe0f7502ef9a73
lcaptain47 <lcaptain47@tamu.edu>
2021-03-17 14:51:10 -0500

    Added user role validations and tests

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,2 +1,5 @@
 class UserRole < ApplicationRecord
+    validates :name, presence: true, uniqueness: {case_sensitive: false}
+    validates :can_create, inclusion: {in: [true,false]}
+    validates :can_delete, inclusion: {in: [true,false]}
 end
---
+++ spec/model/user_role_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe UserRole do
+    it "is valid with valid input" do
+        role = UserRole.new(name: "President", can_create: true, can_delete: true)
+        expect(role).to be_valid
+        role = UserRole.new(name: "User", can_create: false, can_delete: false)
+        expect(role).to be_valid
+    end
+
+    it "is not valid without a name" do
+        role = UserRole.new(can_create: true, can_delete: true)
+        expect(role).to_not be_valid
+    end
+
+    it "is not valid with a duplicate name" do
+        role = UserRole.new(name: "President", can_create: true, can_delete: true)
+        role.save
+        role = UserRole.new(name: "President", can_create: true, can_delete: true)
+        expect(role).to_not be_valid
+    end
+
+    it "is valid with a unique name and valid input" do
+        role = UserRole.new(name: "President", can_create: true, can_delete: true)
+        role.save
+        role2 = UserRole.new(name: "President2", can_create: true, can_delete: true)
+        expect(role2).to be_valid
+    end
+
+    it "is not valid without a can_create entry" do
+        role = UserRole.new(name: "President", can_delete: true)
+        expect(role).to_not be_valid
+    end
+
+    it "is not valid without a can_delete entry" do
+        role = UserRole.new(name: "President", can_create: true)
+        expect(role).to_not be_valid
+    end
+end
