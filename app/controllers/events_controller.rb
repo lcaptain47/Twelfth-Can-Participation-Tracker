b0a7a9409b64bb956fb2e76a0d467e3c8ddad0e7
lcaptain47 <lcaptain47@tamu.edu>
2021-03-29 16:26:53 -0500

    Set up timeslots matrix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,6 +11,56 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     @timeslots = @event.timeslots
     @timeslots = Timeslot.where(event_id: params[:id]).order(time: 'asc', role: 'asc')
+
+    @timeslots_matrix = []
+
+    role_count = 1
+
+    row = []
+
+    #Adds timeslots for an easy to use matrix that the view will use
+    total_roles = @event.volunteers + @event.front_desks + @event.runners
+    @timeslots.each do |timeslot|
+      
+      if role_count <= total_roles
+        row.push(timeslot)
+        role_count += 1
+      else
+        @timeslots_matrix.push(row)
+        row = []
+        row.push(timeslot)
+        role_count = 2
+      end
+    end
+
+    @timeslots_matrix.push(row)
+
+    @header = ['time']
+
+
+
+    counter = 0
+    @event.front_desks.times do
+      counter += 1
+      header_part = "Front Desk #{counter}"
+      @header.push(header_part)
+    end
+    counter = 0
+
+    @event.runners.times do
+      counter += 1
+      header_part = "Runner #{counter}"
+      @header.push(header_part)
+    end
+    counter = 0
+    @event.volunteers.times do
+      counter += 1
+      header_part = "Volunteer #{counter}"
+      @header.push(header_part)
+    end
+
+    byebug
+
   end
 
   # Prepares form for a new event
