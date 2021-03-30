d56c6cd7b80fc895420d2eaed66ea431bd1674fa
lcaptain47 <lcaptain47@tamu.edu>
2021-03-30 12:00:48 -0500

    Fixed rspecs

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -3,7 +3,9 @@
 class Timeslot < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
-  # validates :time, uniqueness: { scope: :event_id }, presence: true
+  validates :time, uniqueness: { scope: [:event_id, :role, :role_number]}, presence: true
   validates :time, presence: true
   validates :duration, presence: true
+  validates :role, presence: true
+  validates :role_number, presence: true
 end
--- spec/model/timeslot_spec.rb
+++ spec/model/timeslot_spec.rb
@@ -9,32 +9,42 @@ RSpec.describe Timeslot do
   end
 
   it 'is valid with valid input' do
-    timeslot = described_class.new(time: '12:00', duration: 60, event: Event.first)
+    timeslot = described_class.new(time: '12:00', duration: 60, event: Event.first, role: 'Volunteer', role_number: 1)
     expect(timeslot).to be_valid
     timeslot.save
-    timeslot = described_class.new(time: '1:00', duration: 60, event_id: Event.first.id)
+    timeslot = described_class.new(time: '1:00', duration: 60, event_id: Event.first.id, role: 'Volunteer', role_number: 2)
     expect(timeslot).to be_valid
   end
 
-  it 'is not valid with a duplicate time' do
-    timeslot = described_class.new(time: '12:00', duration: 60, event_id: Event.first.id)
+  it 'is not valid with a duplicate time, role, and role number' do
+    timeslot = described_class.new(time: '12:00', duration: 60, event_id: Event.first.id, role: 'Volunteer', role_number: 1)
     timeslot.save
-    timeslot = described_class.new(time: '12:00', duration: 60, event_id: Event.first.id)
+    timeslot = described_class.new(time: '12:00', duration: 60, event_id: Event.first.id, role: 'Volunteer', role_number: 1)
     expect(timeslot).not_to be_valid
   end
 
   it 'is not valid without a duration' do
-    timeslot = described_class.new(time: '12:00', event_id: Event.first.id)
+    timeslot = described_class.new(time: '12:00', event_id: Event.first.id, role: 'Volunteer', role_number: 1)
     expect(timeslot).not_to be_valid
   end
 
   it 'is not valid without a time' do
-    timeslot = described_class.new(duration: 60, event_id: Event.first.id)
+    timeslot = described_class.new(duration: 60, event_id: Event.first.id, role: 'Volunteer', role_number: 1)
     expect(timeslot).not_to be_valid
   end
 
   it 'is not valid without an event' do
-    timeslot = described_class.new(time: '12:00', duration: 60)
+    timeslot = described_class.new(time: '12:00', duration: 60, role: 'Volunteer', role_number: 1)
     expect(timeslot).not_to be_valid
+  end
+
+  it 'is not valid without a role' do
+    timeslot = described_class.new(time: '12:00', duration: 60, event_id: Event.first.id, role_number: 1)
+    expect(timeslot).to_not be_valid
+  end
+
+  it 'is not valid without a role number' do
+    timeslot = described_class.new(time: '12:00', duration: 60, event_id: Event.first.id, role: 'Volunteer')
+    expect(timeslot).to_not be_valid
   end
 end
