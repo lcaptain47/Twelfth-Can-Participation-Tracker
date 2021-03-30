3beefec4afc7c16cacc815990a262d3db53abc61
aurelio8321 <alejandro_8321@tamu.edu>
2021-03-29 20:42:13 -0500

    updated rspec test cases

--- spec/features/confirm_delete_spec.rb
+++ spec/features/confirm_delete_spec.rb
@@ -4,7 +4,7 @@ require 'faker'
 
 RSpec.describe 'Tests the confirm delete feature' do
 
-    it 'displays a confirmation message if user is an officer' do
+    it 'displays a confirmation message if user is an officer, and deletes event if user accepts confirmation' do
         test_user = OmniAuth.config.mock_auth[:google_oauth2]
         User.create(uid: test_user[:uid], full_name: test_user[:info][:name], email: test_user[:info][:email], avatar_url: test_user[:info][:image], user_role: UserRole.find_by(name: 'Officer'))
         Event.create(name: 'Test', date: '12-01-2021')
@@ -24,7 +24,28 @@ RSpec.describe 'Tests the confirm delete feature' do
 
     end
 
-    it 'displays a confirmation message if user is the president' do
+    it 'displays a confirmation message if user is an officer, and does NOT delete event if user declines confirmation' do
+        test_user = OmniAuth.config.mock_auth[:google_oauth2]
+        User.create(uid: test_user[:uid], full_name: test_user[:info][:name], email: test_user[:info][:email], avatar_url: test_user[:info][:image], user_role: UserRole.find_by(name: 'Officer'))
+        Event.create(name: 'Test1', date: '12-01-2021')
+        Timeslot.create(time: '12:00', duration: 60, event: Event.first)
+
+        visit '/'
+        click_link 'Sign in'
+        click_link 'Test1'
+        expect(page).to have_content('Delete')
+        
+        page.dismiss_confirm do
+            click_link 'Delete'
+        end
+
+        sleep(1)
+
+        expect(page).to have_content('Test1')
+
+    end
+
+    it 'displays a confirmation message if user is the president, and deletes event if user accepts confirmation' do
         test_user = OmniAuth.config.mock_auth[:google_oauth2]
         User.create(uid: test_user[:uid], full_name: test_user[:info][:name], email: test_user[:info][:email], avatar_url: test_user[:info][:image], user_role: UserRole.find_by(name: 'President'))
         Event.create(name: 'Test2', date: '12-01-2021')
@@ -46,12 +67,32 @@ RSpec.describe 'Tests the confirm delete feature' do
         
     end
 
+    it 'displays a confirmation message if user is the president, and does NOT delete event if user declines confirmation' do
+        test_user = OmniAuth.config.mock_auth[:google_oauth2]
+        User.create(uid: test_user[:uid], full_name: test_user[:info][:name], email: test_user[:info][:email], avatar_url: test_user[:info][:image], user_role: UserRole.find_by(name: 'President'))
+        Event.create(name: 'Test3', date: '12-01-2021')
+        Timeslot.create(time: '12:00', duration: 60, event: Event.first)
+
+        visit '/'
+        click_link 'Sign in'
+        click_link 'Test3'
+        expect(page).to have_content('Delete')
+        
+        page.dismiss_confirm do
+            click_link 'Delete'
+        end
+
+        sleep(1)
+
+        expect(page).to have_content('Test3')
+    end
+
     it 'hides the delete button if user is not an officer or president' do
-        Event.create(name: 'Test', date: '12-01-2021')
+        Event.create(name: 'Test4', date: '12-01-2021')
         Timeslot.create(time: '12:00', duration: 60, event: Event.first)
         visit '/'
         click_link 'Sign in'
-        click_link 'Test'
+        click_link 'Test4'
         expect(page).not_to have_content('Delete')
         
         
