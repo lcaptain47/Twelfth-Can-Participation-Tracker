3adef905124d20fa37a00de1dc3de055c00310a1
Scott <scott@tamu.edu>
2021-04-01 16:19:38 -0500

    Before Scrum Meeting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,16 @@ class UsersController < ApplicationController
     if current_user.user_role.can_delete_users
       @user.each do
       if user != current_user
-        @user.delete
+        user.destroy
       end
 
     #if user is not an officer or preseident delete them
     end
   end
 
+  def destroy
+    @user.destroy
   end
+end
   
 end
