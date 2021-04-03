fdb0b345b935c529fa18a7bb7cd1d0bebd39941b
Scott <scott@tamu.edu>
2021-04-03 12:06:45 -0500

    Delete User funciton made

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,25 @@ class UsersController < ApplicationController
   end
 
   def wipe_all
-    if current_user.user_role.can_delete_users
-      @user.each do
-      if user != current_user
-        user.delete
-      end
+    return unless current_user.user_role.can_delete_users
 
-    #if user is not an officer or preseident delete them
+    user_role = UserRole.find_by(name: 'User')
+
+    users = user_role.users
+    users.each(&:destroy)
+    flash[:notice] = "Sytem Wiped of all normal users"
+    redirect_to root_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user.user_role.can_delete_users
+      flash[:notice] = "#{@user.full_name} Deleted"
+      @user.destroy
+
+      redirect_to root_path
+    else
+      redirect_to user_path(@user)
     end
   end
-
-  def delete
-    @user.delete
-  end
-end
-  
 end
