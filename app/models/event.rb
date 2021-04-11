387828d263a51f3f2da31f9e15b387c947c67697
lcaptain47 <lcaptain47@tamu.edu>
2021-04-11 11:19:46 -0500

    Added more styling to forms

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ApplicationRecord
   has_many :timeslots, dependent: :destroy
   validates :date, presence: true
   validates :name, presence: true
+  validates :description, length: {maximum: 250}
 
   def default_values
     self.volunteers = volunteers.presence || 0
